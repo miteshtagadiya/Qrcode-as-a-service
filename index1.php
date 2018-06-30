46bb41f60c7994638888b5027157c6e041d50eb9
Mitesh <miteshtagadiya@gmail.com>
2018-06-30 18:00:18 +0530

    initial commit

--- index1.php
+++ index1.php
@@ -1,13 +1,15 @@
 <?php
 
 include "phpqrcode/qrlib.php";
-$backColor = "green";
-$foreColor = "blue";
+//include "config.php"; 
 
 
 	$str = $_POST['str'];
-// create a QR Code with this text and display it
-QRcode::png($str,$backColor,$foreColor);
+// // create a QR Code with this text and display it
+// QRcode::png($str);
+// QRcode::png($str,QR_ECLEVEL_L);
+// QRcode::png($str,QR_ECLEVEL_M);
+// QRcode::png($str,QR_ECLEVEL_Q);
+QRcode::png($str);
 
-}
 ?>
